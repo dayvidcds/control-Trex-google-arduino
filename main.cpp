d98e56aef0e87261fd860ba15118bb1baefa9096
Dayvid <dayvid.clementino@gmail.com>
2018-10-19 18:05:20 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
-#include <windows.h>
+#define _WIN32_WINNT 0x0500
+#include "Windows.h"
+#define _WIN32_WINNT 0x0500
 #include <string.h>
 #include "SerialPort.hpp"
 
 using namespace std;
-
-char *port_name = "\\\\.\\COM9";
-
-char incomingData[MAX_DATA_LENGTH];
 
 void pular() {
 	INPUT pulo = { 0 };
@@ -19,14 +17,22 @@ void pular() {
 	SendInput(1, &pulo, sizeof(INPUT));
 }
 
-int main() {
+int main(int argc, char **argv) {
+	char *port_name = argv[1];
+	char incomingData[MAX_DATA_LENGTH];
 	SerialPort arduino(port_name);
 	if (arduino.isConnected()) cout << "Connection Established" << endl;
 	else cout << "ERROR, check port name";
 	
 	while (arduino.isConnected()){
 	  int read_result = arduino.readSerialPort(incomingData, MAX_DATA_LENGTH);
-	  puts(incomingData);
+	  if (read_result) {
+		pular();
+		cout << "pulando!!!";
+	  }
+	  else {
+		cout << "Aguardando..." << endl;
+	  }
 	  Sleep(10);
 	}
 	return 0;
